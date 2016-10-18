e2fd6f6bc1494e9a8f94c19f98ea194e1e06b5ce
geoffroy.destaintot <geoffroy.destaintot@gmail.fr>
2016-10-18 14:48:27 +0200

    packaging

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+import os
+
+with open(os.path.join("outil", "version.py")) as f:
+    version = f.read().split("=")[1].strip().strip("'").strip('"')
+
+
+def _get_req_list(file_name):
+    with open(os.path.join(file_name)) as f:
+        return [str(r) for r in parse_requirements(f.read())]
+
+
+setup(
+    name='openergy',
+
+    version=version,
+
+    packages=find_packages(),
+
+    author="Geoffroy d'Estaintot",
+
+    author_email="geoffroy.destaintot@openergy.fr",
+
+    long_description=open('README.md').read(),
+
+    install_requires=_get_req_list("requirements-conda.txt"),
+
+    url='https://bitbucket.org/openergy/outil',
+
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Research & Development",
+        "Natural Language :: French",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Scientific/Engineering :: Data processing",
+    ],
+    package_data={'openergy': ['*.txt']},
+
+    include_package_data=True
+)
