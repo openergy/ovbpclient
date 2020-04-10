cadb683cc4d20dfdf6ad90598bbfb43fe0d99239
geoffroy.destaintot <geoffroy.destaintot@openergy.fr>
2020-04-10 11:04:33 +0200

    setup.py debug

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import setup, find_packages
 from pkg_resources import parse_requirements
 import os
 
-with open(os.path.join("openergy", "version.py")) as f:
+with open(os.path.join("ovbpclient", "version.py")) as f:
     version = f.read().split("=")[1].strip().strip("'").strip('"')
 
 with open("requirements.txt", "r") as f:
     requirements = [str(r).replace("pytables", "tables") for r in parse_requirements(f.read())]
 
 setup(
-    name='openergy',
+    name="ovbpclient",
     version=version,
     packages=find_packages(),
     author="Geoffroy d'Estaintot",
     author_email="geoffroy.destaintot@openergy.fr",
     long_description=open('README.md').read(),
     install_requires=requirements,
-    url='https://github.com/openergy/openergy',
+    url='https://github.com/openergy/ovbpclient',
     classifiers=[
         "Programming Language :: Python",
         "Development Status :: 5 - Production/Stable",
@@ -27,6 +27,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    package_data={'openergy': ['*.txt']},
+    package_data={"ovbpclient": ['*.txt']},
     include_package_data=True
 )
